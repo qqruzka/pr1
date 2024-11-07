9e2e3399fa69ab894e549f3913fe147cdd66c4a3
qqruzka <162001412+qqruzka@users.noreply.github.com>
2024-11-07 16:46:29 +0700

    Update dbase.h

--- dbase.h
+++ dbase.h
@@ -1,53 +1,51 @@
 #ifndef DBASE_H
 #define DBASE_H
 
-#include <iostream>
-#include <fstream>
-#include <direct.h>
+#include <string>
+#include "Array.h"
 #include "json.hpp"
-#include <iomanip>
-#include <stdexcept>
-#include "array.h"
 
 using namespace std;
 using json = nlohmann::json;
 
+// Узел списка для хранения таблиц
 struct Node {
     string name;
     Array data;
     Node* next;
 
-    Node(const string& name);
+    Node(const string& name) : name(name), next(nullptr) {}
 };
 
+// Структура базы данных
 struct dbase {
     string filename; 
     string schema_name;
     Node* head;
     int current_pk;
 
-    dbase();
+    dbase() : head(nullptr), current_pk(0) {}
     ~dbase();
-
-    void lockPrimaryKey();
-    void unlockPrimaryKey();
-    void loadSchema(const string& schema_file);
-    void initializePrimaryKey();
-    void updatePrimaryKey();
-    size_t getColumnCount(const string& table);
-    void createDirectories(const json& structure);
-    void addNode(const string& table_name);
-    Node* findNode(const string& table_name);
-    void load();
-    bool applyAndFilters(const json& entry, const pair<string, string> filters[], int filter_count);
-    bool applyOrFilters(const json& entry, const pair<string, string> filters[], int filter_count);
-    void select(const string& column, const string& table, const pair<string, string> filters[], int filter_count, const string& filter_type);
-    void saveSingleEntryToCSV(const string& table, const json& entry);
-    void insert(const string& table, json entry);
-    void deleteRow(const string& column, const string& value, const string& table);
-    void rewriteCSV(const string& table);
 };
 
+// Функции для работы с dbase
+void lockPrimaryKey(dbase& db);
+void unlockPrimaryKey(dbase& db);
+void initializePrimaryKey(dbase& db);
+void createDirectories(dbase& db, const json& structure);
+void addNode(dbase& db, const string& table_name);
+void loadSchema(dbase& db, const string& schema_file);
+void updatePrimaryKey(dbase& db);
+Node* findNode(dbase& db, const string& table_name);
+size_t getColumnCount(dbase& db, const string& table);
+void load(dbase& db);
+bool applyAndFilters(const json& entry, const pair<string, string> filters[], int filter_count);
+bool applyOrFilters(const json& entry, const pair<string, string> filters[], int filter_count);
+void select(dbase& db, const string& column, const string& table, const pair<string, string> filters[], int filter_count, const string& filter_type);
+void saveSingleEntryToCSV(dbase& db, const string& table, const json& entry);
+void insert(dbase& db, const string& table, json entry);
+void rewriteCSV(dbase& db, const string& table);
+void deleteRow(dbase& db, const string& column, const string& value, const string& table);
 void executeQuery(dbase& db, const string& query);
 
 #endif // DBASE_H
