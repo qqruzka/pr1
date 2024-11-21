2567f9e459d20c555a2564283e8d0800a07b0a10
qqruzka <162001412+qqruzka@users.noreply.github.com>
2024-11-21 19:18:54 +0700

    Update vector.h

--- array.h
+++ array.h
@@ -1,21 +1,39 @@
-#ifndef ARRAY_H
-#define ARRAY_H
-
-#include <string>
-#include <stdexcept>
-
-using namespace std;
-
-struct Array {
-    string* arr;
+#pragma once
+template<typename T>
+class Vector {
+private:
+    T* arr;
     size_t capacity;
     size_t size;
 
-    Array();
-    ~Array();
-    void addEnd(const string& value);
-    string get(size_t index) const;
-    size_t getSize() const;
-};
+public:
+    Vector() : capacity(10), size(0) {
+        arr = new T[capacity];
+    }
 
-#endif // ARRAY_H
+    ~Vector() {
+        delete[] arr;
+    }
+
+    void addEnd(const T& value) {
+        if (size >= capacity) {
+            capacity *= 2;
+            T* new_arr = new T[capacity];
+            for (size_t i = 0; i < size; ++i) {
+                new_arr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = new_arr;
+        }
+        arr[size++] = value;
+    }
+
+    T get(size_t index) const {
+        if (index >= size) throw std::out_of_range("Index out of range");
+        return arr[index];
+    }
+
+    size_t getSize() const {
+        return size;
+    }
+};
