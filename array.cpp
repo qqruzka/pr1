3955125852d030268afa841b9c72008c932ee6f2
qqruzka <162001412+qqruzka@users.noreply.github.com>
2024-11-21 19:17:38 +0700

    Update vector.cpp

--- array.cpp
+++ array.cpp
@@ -1,31 +1,39 @@
-#include "array.h"
+#pragma once
+template<typename T>
+class Vector {
+private:
+    T* arr;
+    size_t capacity;
+    size_t size;
 
-Array::Array() : capacity(10), size(0) {
-    arr = new string[capacity];
-}
-
-Array::~Array() {
-    delete[] arr;
-}
-
-void Array::addEnd(const string& value) {
-    if (size >= capacity) {
-        capacity *= 2;
-        string* new_arr = new string[capacity];
-        for (size_t i = 0; i < size; ++i) {
-            new_arr[i] = arr[i];
-        }
-        delete[] arr;
-        arr = new_arr;
+public:
+    Vector() : capacity(10), size(0) {
+        arr = new T[capacity];
     }
-    arr[size++] = value;
-}
 
-string Array::get(size_t index) const {
-    if (index >= size) throw out_of_range("Index out of range");
-    return arr[index];
-}
+    ~Vector() {
+        delete[] arr;
+    }
 
-size_t Array::getSize() const {
-    return size;
-}
+    void addEnd(const T& value) {
+        if (size >= capacity) {
+            capacity *= 2;
+            T* new_arr = new T[capacity];
+            for (size_t i = 0; i < size; ++i) {
+                new_arr[i] = arr[i];
+            }
+            delete[] arr;
+            arr = new_arr;
+        }
+        arr[size++] = value;
+    }
+
+    T get(size_t index) const {
+        if (index >= size) throw std::out_of_range("Index out of range");
+        return arr[index];
+    }
+
+    size_t getSize() const {
+        return size;
+    }
+};
